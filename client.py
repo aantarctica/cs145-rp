d152f234c95848b46041dd969ff8e6cb00c5222d
Mutya <mcgulapa@up.edu.ph>
2022-08-16 16:46:00 +0800

    Refactored code to OOP

--- client.py
+++ client.py
@@ -1,85 +1,101 @@
 import socket
-import sympy
-
 # SSH to your AWS instance
 # ssh -i "keypair.pem" ubuntu@54.255.247.86
 
-UDP_IP_ADDRESS = "10.0.5.69"
-UDP_PORT_NO = 9000
 
-UNIQUE_ID = "d5f5c97c"
+class packet:
+    def __init__(self):
+        self.UNIQUE_ID = "d5f5c97c"
+        self.TRANSACTION_ID = "YYYYYYYY"
+        self.FLAG = "8"
+        self.PULL_BYTE = "VVVVV"
+        self.PULL_SIZE = "WWWWW"
+        self.UIN = "TTTTTTT"
+        self.UIN_ANS = "0"
+        self.DATA = "0"
 
-# INITIATE PACKET
-TRANSACTION_ID = "YYYYYYYY"
-FLAG = "8"
-PULL_BYTE = "VVVVV"
-PULL_SIZE = "WWWWW"
-UIN = "TTTTTTT"
-UIN_ANS = "0"
-DATA = "0"
+    def setTransID(self, TRANSACTION_ID):
+        self.TRANSACTION_ID = TRANSACTION_ID
+
+    def setFlag(self, FLAG):
+        self.FLAG = FLAG
+
+    def setPullByte(self, PULL_BYTE):
+        self.PULL_BYTE = PULL_BYTE
+
+    def setPullSize(self, PULL_SIZE):
+        self.PULL_SIZE = PULL_SIZE
+
+    def setUIN(self, UIN):
+        self.UIN = UIN
+
+    def setUINAns(self, UIN_ANS):
+        self.UIN_ANS = UIN_ANS
+
+    def setData(self, DATA):
+        self.DATA = DATA
 
 
-INITIATE_PACKET = UNIQUE_ID + TRANSACTION_ID + FLAG + \
-    PULL_BYTE + PULL_SIZE + UIN + UIN_ANS + "/" + DATA
+class sender:
+    def __init__(self):
+        self.UDP_IP_ADDRESS = "10.0.5.69"
+        self.UDP_PORT_NO = 9000
+        self.clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.clientSock.bind(('', 6703))
 
-# print(packet)
+        self.PACKET = packet()
 
+    def sendPacket(self, type):
+        PACKET = self.PACKET
 
-Message = INITIATE_PACKET.encode()
+        if type == "INITIATE":
+            pass
+        elif type == "PULL":
+            PACKET.setFlag("4")
+            PACKET.setPullByte("00000")
+            PACKET.setPullSize("00002")
+        elif type == "ACK":
+            pass
+        elif type == "SUBMIT":
+            pass
+        else:
+            print("ERROR: Packet type not specified.")
 
-clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-clientSock.bind(('', 6703))
+        self.PAYLOAD = f"{PACKET.UNIQUE_ID}{PACKET.TRANSACTION_ID}{PACKET.FLAG}{PACKET.PULL_BYTE}{PACKET.PULL_SIZE}{PACKET.UIN}{PACKET.UIN_ANS}/{PACKET.DATA}"
+        self.clientSock.sendto(self.PAYLOAD.encode(),
+                               (self.UDP_IP_ADDRESS, self.UDP_PORT_NO))
 
-clientSock.sendto(Message, (UDP_IP_ADDRESS, UDP_PORT_NO))
+    def receiveAccept(self):
+        PACKET = self.PACKET
+        data, _ = self.clientSock.recvfrom(1024)
 
-
-# WAIT FOR SERVER: ACCEPT
-while True:
-    data, addr = clientSock.recvfrom(1024)
-    if len(data) > 0:
         print(data.decode())
-        TRANSACTION_ID = str(data.decode())
-        break
 
-# PULL PACKET
-FLAG = "4"
-PULL_BYTE = "00000"
-PULL_SIZE = "00002"
-UIN = "TTTTTTT"
-UIN_ANS = "0"
-DATA = "0"
+        TRANSACTION_ID = data.decode()
+        PACKET.setTransID(TRANSACTION_ID)
 
+    def receiveData(self):
+        PACKET = self.PACKET
+        data, _ = self.clientSock.recvfrom(1024)
 
-PULL_PACKET = UNIQUE_ID + TRANSACTION_ID + FLAG + \
-    PULL_BYTE + PULL_SIZE + UIN + UIN_ANS + "/" + DATA
-Message = PULL_PACKET.encode()
+        print(data.decode())
 
-clientSock.sendto(Message, (UDP_IP_ADDRESS, UDP_PORT_NO))
-
-
-# WAIT FOR SERVER: DATA
-while True:
-    data, addr = clientSock.recvfrom(1024)
-    if len(data) > 0:
         SERVER_DATA = data.decode()
-        print(SERVER_DATA)
-        break
+
+        UIN = SERVER_DATA[14:21]
+        PACKET.setUIN(UIN)
+
+        CHQ, ENCDATA = SERVER_DATA[24:].split("DATA")
+        CHQ = int(CHQ)
+
+        print(f"UIN: {UIN}\nCHQ: {CHQ}\nDATA: {ENCDATA}\n")
+
+    def beginTransaction(self):
+        self.sendPacket("INITIATE")
+        self.receiveAccept()
+        self.sendPacket("PULL")
+        self.receiveData()
 
 
-# PARSE SERVER DATA
-
-UIN = SERVER_DATA[14:21]
-CHQ, ENCDATA = SERVER_DATA[24:].split("DATA")
-CHQ = int(CHQ)
-
-i = 1
-while True:
-    if CHQ % sympy.prime(i) == 0:
-        j = CHQ / sympy.prime(i)
-        break
-    i += 1
-    print(sympy.prime(i), end="\t")
-
-shift = i % 26
-
-print(f"UIN: {UIN}\nCHQ: {CHQ}\nDATA: {ENCDATA}\n")
+SENDER = sender()
+SENDER.beginTransaction()
