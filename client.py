23372eb37cdf31ff2c8320628ecc65910c34d893
Mutya <mcgulapa@up.edu.ph>
2022-08-27 11:09:15 +0800

    Added comments

--- client.py
+++ client.py
@@ -1,4 +1,3 @@
-from http import client
 import socket
 import math
 import random
@@ -6,15 +5,18 @@ import time
 import select
 import argparse
 
-# SSH to your AWS instance
-# ssh -i "keypair.pem" ubuntu@
-# git config --global credential.helper store
-
-# python3 client.py -a 10.0.5.69 -s 9000 -c 6703 -i d5f5c97c -d 5
+# THE `packet` CLASS
+# Contains the necessary information stored
+# in a packet being sent by the client
+# (student id, transaction id, etc)
 
 
 class packet:
+    # Initializes the packet instance
     def __init__(self, UNIQUE_ID):
+        # Initialize the packet as an INITIATE PACKET
+        # Attributes included in the actual packet
+        # being sent
         self.UNIQUE_ID = UNIQUE_ID
         self.TRANSACTION_ID = "YYYYYYYY"
         self.FLAG = "8"
@@ -23,159 +25,229 @@ class packet:
         self.UIN = "TTTTTTT"
         self.UIN_ANS = "0"
         self.DATA = ""
-        self.TEMPDATA = ""
-        self.SHIFT = 0
-        self.DONE = False
 
+        # Attributes necessary for the algorithms being used
+        # Temporarily stores the encrypted data while <END> is not yet pulled.
+        self.TEMPDATA = ""
+        # The shift value computed for each UIN. Used by decodeData method.
+        self.SHIFT = 0
+        self.DONE = False   # Determines if the last data packet is received.
+
+    # Sets the value of the Transaction ID
     def setTransID(self, TRANSACTION_ID):
         self.TRANSACTION_ID = TRANSACTION_ID
 
+    # Sets the value of the Flag
     def setFlag(self, FLAG):
         self.FLAG = FLAG
 
+    # Sets the value of the Pull Byte
     def setPullByte(self, PULL_BYTE):
         self.PULL_BYTE = PULL_BYTE
 
+    # Sets the value of the Pull Size
     def setPullSize(self, PULL_SIZE):
         self.PULL_SIZE = PULL_SIZE
 
+    # Sets the value of the UIN
     def setUIN(self, UIN):
         self.UIN = UIN
 
+    # Sets the value of the UIN Answer
     def setUINAns(self, UIN_ANS):
         self.UIN_ANS = UIN_ANS
 
+    # Sets the value of the Shift
     def setShift(self, SHIFT):
         self.SHIFT = SHIFT
 
+    # Appends encrypted to TEMPDATA
     def appendData(self, TEMPDATA):
         self.TEMPDATA += TEMPDATA
 
+    # Copies the value of TEMPDATA to DATA
     def setData(self, DATA):
         self.DATA = DATA
 
+    # Decodes the encrypted data using Caesar Cipher
     def decodeData(self, ENCRYPTED):
         DECRYPTED = ""
         base_capital = ord('A')
         base_small = ord('a')
 
+        # Decrypts the encrypted string character by character
         for char in ENCRYPTED:
+            # Assures that the decryption is case-sensitive
             if char.isupper():
+                # Appends each decrypted character to DECRYPTED variable
                 DECRYPTED += chr((ord(char) - base_capital -
                                  self.SHIFT) % 26 + base_capital)
             else:
                 DECRYPTED += chr((ord(char) - base_small -
                                  self.SHIFT) % 26 + base_small)
 
-        # print(f"\t\tENCRYPTED: {ENCRYPTED}\n\t\tDECRYPTED: {DECRYPTED}")
+        # Appends the completed DECRYPTED string to TEMPDATA
         self.TEMPDATA += DECRYPTED
 
+    # Sets DONE to True
     def setDone(self):
         self.DONE = True
 
 
+# THE `sender` CLASS
+# Handles the sending and receiving of
+# packets.
 class sender:
+    # Initializes the sender instance
     def __init__(self, args):
+
+        # Initializes the server address and port from
+        # parsed arguments.
         self.UDP_IP_ADDRESS = args.address
         self.UDP_PORT_NO = args.server_port
+
+        # Initializes the client socket variable.
+        # Socket is binded to the client port number acquired.
         self.clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.clientSock.bind(('', args.client_port))
 
+        # Sets PACKET_ID to the student's unique ID.
         self.PACKET_ID = args.unique_id
-        self.ITERATIONS = args.debug
 
+        # Initializes variables for a new transaction
         self.PULL_SIZE = 1
         self.PULL_BYTE = 0
         self.MAX_PULL_SIZE = 1000
+        # Determines if pull window is exceeded
         self.WINDOW_EXCEEDED = False
+        # Sets the start time of the transaction
         self.TRANSACTION_START_TIME = time.time()
+
+        # Sets the start time of the pull packet.
         self.PULL_START_TIME = 0
 
+    # Converts integer pull values to string
+    # to accommodate the format of the packet
     def getPullValues(self):
         PACKET = self.PACKET
 
+        # Gets the string length of the converted integers
         PSIZE_STR = str(self.PULL_SIZE)
         PBYTE_STR = str(self.PULL_BYTE)
 
+        # Computes the number of leading zeroes
+        # and sets value of packet PULL_SIZE and PULL_BYTE
         zeroes = 5 - len(PSIZE_STR)
         PACKET.setPullSize("0" * zeroes + PSIZE_STR)
 
         zeroes = 5 - len(PBYTE_STR)
         PACKET.setPullByte("0" * zeroes + PBYTE_STR)
 
+    # Modifies the pull size and pull byte accordingly
+    # to accommodate for the next pull packet
     def handleNextPull(self):
         if self.WINDOW_EXCEEDED:
+            # If Pull Window is exceeded:
+            # decreases the pull size and
+            # sets the maximum pull size
+
             self.PULL_SIZE -= 1
             self.MAX_PULL_SIZE = self.PULL_SIZE
 
+            # Reverts the Pull Window Exceeded
+            # value to false
             self.WINDOW_EXCEEDED = False
 
         elif self.PULL_SIZE < self.MAX_PULL_SIZE - 1:
+            # If Pull size is one less than the maximum,
+            # shifts the pull byte by the current pull size
+            # then increments the pull size
+
             self.PULL_BYTE += self.PULL_SIZE
             self.PULL_SIZE += 1
 
         else:
+            # If pull size is already at the maximum,
+            # only the pull byte is updated to prevent
+            # from reaching timeout again
             self.PULL_BYTE += self.PULL_SIZE
 
+    # Handles the actual sending of packet to the server
     def sendPacket(self, type):
         PACKET = self.PACKET
 
         if time.time() - self.TRANSACTION_START_TIME > 110:
+            # Submits packet before transaction time
+            # reaches 120 seconds.
+
+            # *Note: This is only a debugging feature to see
+            # if the client is able to send at least 50% of the
+            # payload correctly. This is irrelevant to the specs
+            # of the final project.
+
             PACKET.setDone()
             return
 
         if type == "INITIATE":
+            # Packet is already initialized as an INITIATE
+            # packet. Modifications unnecessary.
             pass
 
         elif type == "PULL":
             PACKET.setFlag("4")
+
+            # Sets the pull byte and pull size of the packet
             self.getPullValues()
 
-            # print("Sending PULL Packet...")
+            # Resets the PULL start time
             self.PULL_START_TIME = time.time()
 
         elif type == "ACK":
             PACKET.setFlag("2")
 
+            # Modifies sender attributes to accommodate
+            # for the next pull packet to be sent
             self.handleNextPull()
-            # print("Sending ACK...")
 
         elif type == "SUBMIT":
             PACKET.setFlag("1")
+
+            # Copies all decrypted data to the packet being submitted
             PACKET.setData(PACKET.TEMPDATA)
             print("Submitting data...")
 
         elif type == "ACK&SUBMIT":
             PACKET.setFlag("3")
 
-            # print("Sending ACK and submitting data...")
-
         else:
             print("ERROR: Packet type not specified.")
 
-        # SEND PAYLOAD
+        # Set packet payload according to the
+        # updated values and send it to server
         self.PAYLOAD = f"{PACKET.UNIQUE_ID}{PACKET.TRANSACTION_ID}{PACKET.FLAG}{PACKET.PULL_BYTE}{PACKET.PULL_SIZE}{PACKET.UIN}{PACKET.UIN_ANS}/{PACKET.DATA}"
-
-        # print(f"{type}:\t{self.PAYLOAD}")
 
         self.clientSock.sendto(self.PAYLOAD.encode(),
                                (self.UDP_IP_ADDRESS, self.UDP_PORT_NO))
 
+    # Accepts the ACCEPT packet from the server.
     def receiveAccept(self):
         PACKET = self.PACKET
         data, _ = self.clientSock.recvfrom(1024)
 
-        SERVER_RESPONSE = data.decode()
+        SERVER_RESPONSE = data.decode()  # Expects a transaction ID
+
         if SERVER_RESPONSE == "Existing alive transaction":
             print("ERROR: Existing alive transaction")
             exit(-1)
 
-        # print(SERVER_RESPONSE)
-
+        # Sets the packet transaction ID
         TRANSACTION_ID = SERVER_RESPONSE
         PACKET.setTransID(TRANSACTION_ID)
 
+    # Subfunction of Pollard's Rho algorithm
     def modular_pow(self, base, exponent, modulus):
+        # code acquired from:
+        # https://www.geeksforgeeks.org/pollards-rho-algorithm-prime-factorization/?fbclid=IwAR3fKScZxZC3J16Zdsks8rPb0NcKhdHjH2LKvMJ5Twef0bNsZWYMiTXML6Y
 
         # initialize result
         result = 1
@@ -194,8 +266,16 @@ class sender:
 
         return result
 
+    # Algorithm for finfing a factor of the CHQ
     def PollardRho(self, n, i):
+        # code acquired from:
+        # https://www.geeksforgeeks.org/pollards-rho-algorithm-prime-factorization/?fbclid=IwAR3fKScZxZC3J16Zdsks8rPb0NcKhdHjH2LKvMJ5Twef0bNsZWYMiTXML6Y
+        # with slight modifications marked with *
+
+        # *DEBUG FEATURE:
+        # determines the total number of iterations
         iterations = i
+
         # no prime divisor for 1
         if (n == 1):
             return n
@@ -205,12 +285,19 @@ class sender:
             return 2
 
         # we will pick from the range [2, N)
+
+        # *STUDENT_NOTE: Code was modified as the
+        # copied version from the website was wrong.
         x = random.randint(random.randint(0, 2), n - 2) + 2
         y = x
 
         # the constant in f(x).
+
         # Algorithm can be re-run with a different c
         # if it throws failure for a composite.
+
+        # *STUDENT_NOTE: Code was modified as the
+        # copied version from the website was wrong.
         c = random.randint(random.randint(0, 1), n - 1) + 1
 
         # Initialize candidate divisor (or result)
@@ -219,6 +306,8 @@ class sender:
         # until the prime factor isn't obtained.
         # If n is prime, return n
         while (d == 1):
+
+            # *Reset if the algorithm takes too long.
             iterations += 1
             if time.time() - self.PULL_START_TIME > 10:
                 self.PULL_START_TIME = time.time()
@@ -241,14 +330,17 @@ class sender:
 
         return d
 
+    # Solves for the two factors of the CHQ
     def getUINAns(self, large_number):
 
-        # print("Calculating factors...")
-
+        # Gets a factor of the large number
         factor = int(self.PollardRho(large_number, 0))
 
+        # Returns a sorted list of the two factors
         return sorted([factor, large_number // factor])
 
+    # Parses the DATA PACKET received by client from
+    # the server
     def parseData(self, SERVER_DATA):
         PACKET = self.PACKET
 
@@ -259,67 +351,90 @@ class sender:
 
         FACTORS = self.getUINAns(int(CHQ))
 
+        # Sets the larger factor as the packet UIN_ANS
         UIN_ANS = int(FACTORS[1])
-        # print(FACTORS)
         PACKET.setUINAns(UIN_ANS)
 
+        # Uses the smaller factor to set the shift value
         PACKET.setShift(int(FACTORS[0] % 26))
 
         if "<END>" in ENCDATA:
-            # print(F"ENCRYPTED:\t{ENCDATA}\n********END OF DATA!********")
+
             PACKET.setDone()
             ENCDATA, _ = ENCDATA.split("<END>")  # Remove <END> from data
 
         PACKET.decodeData(ENCDATA)
 
-        # print(
-        #     f"TRANSACTION_ID:\t{PACKET.TRANSACTION_ID}\nUIN:\t{PACKET.UIN}\nCHQ:\t{CHQ}\nENCDATA:\t{ENCDATA}\nUIN_ANS:\t{PACKET.UIN_ANS}\nSHIFT:\t{PACKET.SHIFT}")
-
+    # Handles receiving the DATA packet sent by the server
     def receiveData(self):
         PACKET = self.PACKET
+
+        # Disables socket blocking to accommodate for
+        # the server mechanism if pull window exceeded.
+        # Timeout is 10 seconds + 1
 
         self.clientSock.setblocking(0)
         ready = select.select([self.clientSock], [], [], 11)
 
         if ready[0]:
             data, _ = self.clientSock.recvfrom(1024)
+
+            # Parses data if valid data received
             self.parseData(data.decode())
         else:
             print("ERROR: Pull Window Exceeded!")
             self.WINDOW_EXCEEDED = True
 
             if time.time() - self.TRANSACTION_START_TIME < 110:
+                # Modifies pull size to handle exceeded error
                 self.handleNextPull()
                 print("Resending PULL Packet...")
+
+                # Resends the same pull packet with a reduced
+                # pull size.
                 self.sendPacket("PULL")
                 self.receiveData()
             else:
+                # *Note: Use of TIME as condition is only a debugging
+                # feature to see if the client is able to send
+                # at least 50% of the payload correctly. This is
+                # irrelevant to the specs of the final project.
+
                 PACKET.setDone()
                 return
 
+        # Enables socket blocking again
         self.clientSock.setblocking(1)
 
+    # Receives the ACK sent by the server
+    # after client sends the submit packet
     def receiveAck(self):
         data, _ = self.clientSock.recvfrom(1024)
         print(f"Ack received:\t{data.decode()}")
 
+    # Executes the sender methods for one complete transaction
     def beginTransaction(self):
         print("New transaction")
-        self.PACKET = packet(self.PACKET_ID)
 
+        # Instantiates a new packet
+        self.PACKET = packet(self.PACKET_ID)
         self.TRANSACTION_START_TIME = time.time()
 
         self.sendPacket("INITIATE")
         self.receiveAccept()
+
+        # Pulls data from the server until last
+        # data packet received
         while not self.PACKET.DONE:
             self.sendPacket("PULL")
             self.receiveData()
             self.sendPacket("ACK")
-            # print("-----------------\n")
+
         self.sendPacket("SUBMIT")
 
+    # Resets sender attributes to accomodate for
+    # a new transaction if -d input is greater than 1
     def endTransaction(self):
-
         print(f"Transaction {self.PACKET.TRANSACTION_ID} DONE!")
         self.receiveAck()
         print("\n\n")
@@ -333,6 +448,7 @@ class sender:
 
 if __name__ == "__main__":
 
+    # Handles arguments in user input
     parser = argparse.ArgumentParser(description='Process arguments')
     parser.add_argument('-a', '--address', type=str,
                         help='IP address of server', default="10.0.5.69")
@@ -346,7 +462,6 @@ if __name__ == "__main__":
                         help='Number of transactions', default=1)
 
     args = parser.parse_args()
-    # print(args)
     SENDER = sender(args)
 
     for i in range(args.debug):
